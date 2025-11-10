8ffedeb287e08ead18cd178ed2ccf0f7b2c7e465
Vaibhav Vishwakarma <vaibh91@Vaibhavs-MacBook-Air.local>
2025-11-10 19:58:50 +0530

    removed compiled files and other ones

--- vendormanagement/management/commands/insert_dummy_data.py
+++ vendormanagement/management/commands/insert_dummy_data.py
@@ -1,8 +1,15 @@
-from vendormanagement.utils.data_utils import insert_dummy_data
+import os
+from vendormanagement.utils.data_utils import insert_dummy_data, get_function_file_path
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     help = 'Insert dummy data into the database from CSV files'
 
     def handle(self, *args, **options):
-        insert_dummy_data()
+        file_base_path = get_function_file_path(insert_dummy_data)
+        file_base_path = "/".join(file_base_path.split("/")[:-2])+"/data"
+        service_file_path = os.path.join(file_base_path, "Services.csv")
+        vendor_file_path = os.path.join(file_base_path, "Vendors.csv")
+        print(f"service_file_path: {service_file_path}")
+        print(f"vendor_file_path: {vendor_file_path}")
+        insert_dummy_data(vendor_file_path, service_file_path)
--- vendormanagement/utils/data_utils.py
+++ vendormanagement/utils/data_utils.py
@@ -1,9 +1,26 @@
 from vendormanagement.models import Vendor, Service
 import pandas as pd
+import os
+import inspect
 
-def insert_dummy_data():
-    df_vendors = pd.read_csv('vendormanagement/data/Vendors.csv')
-    df_services = pd.read_csv('vendormanagement/data/Services.csv')
+
+
+# instead of absolute path use relative path from this files path itself
+
+def get_function_file_path(func):
+    """
+    Returns the absolute path of the file where the given function is defined.
+    """
+    if not inspect.isfunction(func):
+        raise TypeError("Input must be a function.")
+    
+    file_path = inspect.getfile(func)
+    return os.path.abspath(file_path)
+
+def insert_dummy_data(vendor_file_path, service_file_path):
+    
+    df_vendors = pd.read_csv(vendor_file_path)
+    df_services = pd.read_csv(service_file_path)
 
     for index, row in df_vendors.iterrows():
         Vendor.objects.create(name=row['name'], contact_person=row['contact_person'], email=row['email'], phone=row['phone'], status=row['status'])
